4f17e41f34e703a39f50d1f81059494c69cb24ed
bishem <aminrsaidali@gmail.com>
2023-02-27 10:45:28 +0100

    removed jsconfig

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import { App } from '@/app';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { App } from '/src/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
