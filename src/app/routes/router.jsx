28208cdd3e093b36e2025c4ee4f37fee37604bd5
Thibault COSATTINI <t.cosattini@gmail.com>
2023-02-27 11:22:10 +0100

    routing

--- src/app/routes/router.jsx
+++ src/app/routes/router.jsx
@@ -3,9 +3,27 @@ import { ErrorView, HomeView } from '../views';
 
 const ROUTES = [
   {
-    path: '/',
+    path: "/",
     element: <HomeView />,
     errorElement: <ErrorView />,
+  },
+  {
+    //Liste des commandes d'un client sélectionné
+    path: "/client/{client}",
+    // element:
+    // errorElement:
+  },
+  {
+    //Ajout d'une commande pour le client sélectionné
+    path: "/client/{client}/add",
+    // element:
+    // errorElement:
+  },
+  {
+    //Modification d'une commande
+    path: "/update/{commande}",
+    // element:
+    // errorElement:
   },
 ];
 
